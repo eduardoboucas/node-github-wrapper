803ee41f44ed1144de85bf727a56ef6d1fd66e16
Eduardo Bouças <mail@eduardoboucas.com>
2015-09-12 16:21:44 +0100

    Add OAuth flow

--- github-api.js
+++ github-api.js
@@ -1,13 +1,17 @@
 var request = require('request');
 var querystring = require('querystring');
 
-module.exports = (function (config) {
+module.exports = (function (githubConfig) {
     var accessToken;
-    var apiRoot = config.apiRoot || 'https://api.github.com';
+    var apiRoot = githubConfig.apiRoot || 'https://api.github.com/';
     var headers = {
         'Accept': 'application/json',
-        'User-Agent': config.appName
-    } || config.headers;
+        'User-Agent': githubConfig.appName
+    } || githubConfig.headers;
+    var clientId = githubConfig.clientId;
+    var clientSecret = githubConfig.clientSecret;
+    var redirectUri = githubConfig.redirectUri;
+    var scopes = githubConfig.scopes;
 
     /*=========================================
     =            Private functions            =
@@ -16,9 +20,9 @@ module.exports = (function (config) {
     function makeRequest(data, callback) {
         data.headers = data.headers || headers;
         data.urlParameters = data.urlParameters || {};
-        data.auth = data.auth || false;
+        data.auth = (data.auth === false) ? false : accessToken;
         data.method = data.method || 'GET';
-
+        
         if (data.auth) {
             data.urlParameters.access_token = accessToken;
         }
@@ -29,9 +33,11 @@ module.exports = (function (config) {
             data.url = data.url + '?' + urlString;
         }
 
+        data.url = (data.url.substring(0, 4) === 'http') ? data.url : apiRoot + data.url;
+
         var payload = {
             headers: data.headers,
-            url: apiRoot + data.url,
+            url: data.url,
             method: data.method
         };
 
@@ -39,16 +45,68 @@ module.exports = (function (config) {
             payload.json = data.body;
         }
 
-        request(payload, callback);
+        if (data.parseJSON) {
+            request(payload, function (err, response, body) {
+                try {
+                    var parsedBody = JSON.parse(body);
+                
+                    callback.call(undefined, err, response, parsedBody);
+                } catch (parseErr) {
+                    callback.call(undefined, parseErr, response, body);
+                }                
+            });          
+        } else {
+            request(payload, callback);
+        }
     }
 
 
     /*========================================
     =            Public functions            =
     ========================================*/
-    
+
+    function getAuthURL() {
+        var variables = {
+            client_id: clientId,
+            redirect_uri: redirectUri,
+            scope: scopes
+        };
+
+        var url = 'https://github.com/login/oauth/authorize?' + querystring.encode(variables);
+
+        // UGLY!
+        if (Array.isArray(scopes)) {
+            url = url.replace(/scope=/g, 'scope[]=');
+        }
+
+        return url;
+    }
+
+    function authorize(code, callback) {
+        post({
+            url: 'https://github.com/login/oauth/access_token',
+            body: {
+                client_id: clientId,
+                client_secret: clientSecret,
+                code: code                
+            }
+        }, function (err, response, body) {
+            setAccessToken(body.access_token);
+
+            if (typeof callback == 'function') {
+                callback.call(undefined, err, response, body);
+            }
+        });
+    }   
+
     function setAccessToken(newAccessToken) {
         accessToken = newAccessToken;
+
+        return this;
+    }
+
+    function getAccessToken() {
+        return accessToken;
     }
 
     function get(data, callback) {
@@ -75,9 +133,11 @@ module.exports = (function (config) {
         return makeRequest(data, callback);
     }
     
-
     return {
+        getAuthURL: getAuthURL,
+        authorize: authorize,
         setAccessToken: setAccessToken,
+        getAccessToken: getAccessToken,
         get: get,
         post: post,
         put: put,
